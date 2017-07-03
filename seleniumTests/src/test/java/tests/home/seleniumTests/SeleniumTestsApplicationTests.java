fb0d1a25f707b204fff66af0e43812dc6f443599
Nikita <kordevg@yandex.ru>
2017-07-03 20:53:54 +0500

    Add files via upload

---
+++ seleniumTests/src/test/java/tests/home/seleniumTests/SeleniumTestsApplicationTests.java
@@ -0,0 +1,16 @@
+package tests.home.seleniumTests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeleniumTestsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
